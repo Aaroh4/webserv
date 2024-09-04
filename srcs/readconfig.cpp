4096f3da241d5ee6f22012af8aa03f34aff98db1
Aaro Hamalainen <aaro.h4malainen@gmail.com>
2024-09-04 12:40:30 +0300

    Made brackets function that can be used recursively

--- srcs/readconfig.cpp
+++ srcs/readconfig.cpp
@@ -6,31 +6,31 @@
 /*   By: ahamalai <ahamalai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 16:18:51 by ahamalai          #+#    #+#             */
-/*   Updated: 2024/09/03 16:52:37 by ahamalai         ###   ########.fr       */
+/*   Updated: 2024/09/04 12:39:08 by ahamalai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ServerInfo.hpp"
 #include <fstream>
 
+void brackets(std::ifstream &configfile, std::string line, std::string type)
+{
+	if (line.find(type) != std::string::npos && line.find("{") != std::string::npos)
+	{
+		for (std::string line; std::getline(configfile, line) && line.find("}") == std::string::npos;)
+		{
+			std::cout << line << std::endl;
+		}
+	}
+}
+
 int	readconfig(std::string name)
 {
 	std::ifstream configfile(name);
-	// bool	foundserver = false;
 
 	for (std::string line; std::getline(configfile, line);)
 	{
-		// if (line.find("server") != std::string::npos && foundserver == false)
-		// 	foundserver = true;
-		// else if (line.find("server") && foundserver != false)
-		// 	return (1);
-		if (line.find("{") != std::string::npos)
-		{
-			for (std::string line; std::getline(configfile, line) && line.find("}") == std::string::npos;)
-			{
-				std::cout << line << std::endl;
-			}
-		}
+		brackets(configfile, line, "server");
 	}
 	return (0);
 }
