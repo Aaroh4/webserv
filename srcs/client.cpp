1d820c9bc7531a7c6dc523afdde6e54fa047de2a
Tomi Kartaslammi <tomi.kartaslammi@gmail.com>
2024-09-06 15:24:15 +0300

    jee

--- srcs/client.cpp
+++ srcs/client.cpp
@@ -6,7 +6,7 @@
 /*   By: tkartasl <tkartasl@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 13:04:20 by tkartasl          #+#    #+#             */
-/*   Updated: 2024/09/06 13:42:28 by tkartasl         ###   ########.fr       */
+/*   Updated: 2024/09/06 14:00:54 by tkartasl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,18 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <stdio.h>
+
 int main()
 {
 	int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
 	sockaddr_in serverAddress;
 	
 	serverAddress.sin_family = AF_INET;
-	serverAddress.sin_port = htons(8000);
-	if (inet_pton(AF_INET, "192.168.0.100", &serverAddress.sin_addr) == 0)
+	serverAddress.sin_port = htons(8080);
+	if (inet_pton(AF_INET, "172.50.70.131", &serverAddress.sin_addr) == 0)
 		return -1;
-	
-	printf("hei\n");
+
 	connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
-	printf("moii\n");
 	const char* request = "GET local_host:8080 HTTP/1.1";
 	
 	send(clientSocket, request, strlen(request), 0);
