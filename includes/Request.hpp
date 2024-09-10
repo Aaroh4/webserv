8fd083f0bfeb3d3c3430cb8a1fd4bcdac51ad4d4
Tomi Kartaslammi <tomi.kartaslammi@gmail.com>
2024-09-10 15:15:09 +0300

    Progress on Request class

--- includes/Request.hpp
+++ includes/Request.hpp
@@ -6,24 +6,29 @@
 /*   By: tkartasl <tkartasl@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 14:30:52 by tkartasl          #+#    #+#             */
-/*   Updated: 2024/09/06 15:22:41 by tkartasl         ###   ########.fr       */
+/*   Updated: 2024/09/09 16:12:11 by tkartasl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef REQUEST_H
 # define REQUEST_H
 # include <string>
+# include <map>
 
 class Request
 {
 	public:
 		Request(std::string request);
 		~Request(void);
-		parseMethod(void);
-		parseUrl(void);
+		void	parseMethod(void);
+		void	parseUrl(void);
+		void parseHeaders(void);
 	private:
-		const std::string _request;
-		const std::string _method;
-		const std::string _url;
-		const std::string _body;
-}
+		std::string _request;
+		std::string _method;
+		std::string _url;
+		std::string _body;
+		std::map<std::string, std::string> _headers;
+};
+
+#endif
--- srcs/Request.cpp
+++ srcs/Request.cpp
@@ -6,7 +6,7 @@
 /*   By: tkartasl <tkartasl@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 14:49:12 by tkartasl          #+#    #+#             */
-/*   Updated: 2024/09/06 15:22:59 by tkartasl         ###   ########.fr       */
+/*   Updated: 2024/09/10 10:27:25 by tkartasl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,31 +17,59 @@ Request::Request(std::string request) : _request(request)
 	return;
 }
 
-Request::Request(void)
+Request::~Request(void)
 {
 	return;
 }
 
-Request::parseMethod(void)
+//Parses method and put's it to string attribute _method, then erases it from the request
+void	Request::parseMethod(void)
 {
-	const char* methods[3] = {"GET", "POST", "DELETE"}
+	const char* methods[3] = {"GET", "POST", "DELETE"};
 	int i = this->_request.find_first_of(" ");
-	std::string temp = this->_request.substr(0, i);
+	this->_method = this->_request.substr(0, i);
 	int index = 0;
 	
 	for (index; index < 3; index++)
 	{
-		if (temp == methods[i])
-		{
-			this->_method = temp;
+		if (this->_method == methods[i])
 			break;
-		}
 	}
 	if (index == 3)
-		//ERROR
+		/*ERROR*/;
+	this->_request.erase(0, this->_method.length() + 1);
 }
 
-Request::parseUrl(void)
+//Parses URI and put's it to string attribute _url, then erases it from the request
+void	Request::parseUrl(void)
 {
-	
+	int i = this->_request.find_first_of(" ");
+
+	this->_url = this->_request.substr(0, i);
+	this->_request.erase(0, this->_url.length() + 1);
+}
+
+//Parses headers line by line and adds the header(before :) and value (after :) to map container attribute _headers
+void Request::parseHeaders(void)
+{
+	std::string line;
+
+	size_t	lineEnd = 0;
+	size_t	i = 0;
+
+	while (line != "\r\n")
+	{
+		lineEnd = this->_request.find_first_of("\n");
+		line = this->_request.substr(0, lineEnd);
+		i = line.find_first_of(":");
+		if (i == std::string::npos)
+		{
+			i = this->_request.find_last_of("\n");
+			this->_request.erase(0, i + 1);
+			break;
+		}
+		this->_headers[line.substr(0, i)] = line.substr(i + 2, lineEnd);
+		this->_request.erase(0, lineEnd + 1);
+	}
+	this->_body = this->_request;
 }
