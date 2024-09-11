9a1133186fea34cf5e653664cbb4e598ceaea85f
aaro <aaro.h4malainen@gmail.com>
2024-09-11 15:12:39 +0300

    made a keep-alive connection work somewhat

--- srcs/ServerManager.cpp
+++ srcs/ServerManager.cpp
@@ -59,61 +59,60 @@ void	ServerManager::start_servers()
 	}
  	while (true)
 	{
-		int pollcount = poll(poll_fds.data(), poll_fds.size(), -1);
+		int pollcount = poll(poll_fds.data(), poll_fds.size(), 100);
 
 		if (pollcount < 0)
 			break ;
-	 for (size_t i = 0; i < poll_fds.size(); i++) 
-	 {
-            if (poll_fds[i].revents & POLLIN) 
-			{
-                if (i < this->get_info().size()) 
+		for (size_t i = 0; i < poll_fds.size(); i++) 
+		{
+				// static int asd;
+				// asd++;
+				// std::cout << asd << ":" << i << std::endl;
+				if (poll_fds[i].revents & POLLIN) 
 				{
-                    int client_socket = accept(poll_fds[i].fd, nullptr, nullptr);
-                    if (client_socket < 0) {
-                        perror("Accept failed");
-                        continue;
-                }
-					fcntl(client_socket, F_SETFL, O_NONBLOCK, FD_CLOEXEC);
-                    struct pollfd client_pollfd;
-                    client_pollfd.fd = client_socket;
-                    client_pollfd.events = POLLIN;
-                    poll_fds.push_back(client_pollfd);
-
-                    std::cout << "New client connected on server " << i << std::endl;
-                } 
-                else {
-                    char buffer[1024] = {0};
-                    int client_socket = poll_fds[i].fd;
-                    int bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
-                    
-                    if (bytes_received <= 0) 
+					if (i < this->get_info().size()) 
 					{
-                        std::cout << "Client disconnected" << std::endl;
-                        close(client_socket);
-                        poll_fds.erase(poll_fds.begin() + i);
-                        i--;
-                    }
+						int client_socket = accept(poll_fds[i].fd, nullptr, nullptr);
+						if (client_socket < 0) {
+							perror("Accept failed");
+							continue;
+					}
+						fcntl(client_socket, F_SETFL, O_NONBLOCK, FD_CLOEXEC);
+						struct pollfd client_pollfd;
+						client_pollfd.fd = client_socket;
+						client_pollfd.events = POLLIN;
+						poll_fds.push_back(client_pollfd);
+						std::cout << "New client connected on server " << i << std::endl;
+					} 
 					else 
 					{
-						std::ifstream index("www/index.html");
-						std::string file;
-						for (std::string line; std::getline(index, line);)
-							file += line;
-                        std::cout << "Message from client: " << buffer << std::endl;
-						   std::string response = "HTTP/1.1 200 OK\n";
-						response += "Content-Type: text/html\n";
-						response += "Content-Length: " + std::to_string(file.length()) + "\n";
-						response += "Connection: close\n\n"; 
-						response += file;
-						send (client_socket, response.c_str(), response.length(), 0);
-						std::cout << "Client disconnected" << std::endl;
-                        close (client_socket);
-                        poll_fds.erase(poll_fds.begin() + i);
-                        i--;
-                    }
-                }
-            }
+						char buffer[1024] = {0};
+						int client_socket = poll_fds[i].fd;
+						int bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
+						
+						if (bytes_received <= 0) 
+						{
+							std::cout << "Client disconnected" << std::endl;
+							close(client_socket);
+							poll_fds.erase(poll_fds.begin() + i);
+							i--;
+						}
+						else 
+						{
+							std::ifstream index("www/index.html");
+							std::string file;
+							for (std::string line; std::getline(index, line);)
+								file += line;
+							std::cout << "Message from client: " << buffer << std::endl;
+							std::string response = "HTTP/1.1 200 OK\n";
+							response += "Content-Type: text/html\n";
+							response += "Content-Length: " + std::to_string(file.length()) + "\n";
+							response += "Keep-Alive: timeout=5, max=100\n\n"; 
+							response += file;
+							send (client_socket, response.c_str(), response.length(), 0);
+						}
+					}
+				}
 		}
 	}
 }
