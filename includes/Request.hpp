1b6f02f61653b6ee3d57beeafa6202f2557cce4c
Tomi Kartaslammi <tomi.kartaslammi@gmail.com>
2024-09-12 12:40:32 +0300

    Updated Request class and added styles.css

--- includes/Request.hpp
+++ includes/Request.hpp
@@ -6,7 +6,7 @@
 /*   By: tkartasl <tkartasl@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 14:30:52 by tkartasl          #+#    #+#             */
-/*   Updated: 2024/09/09 16:12:11 by tkartasl         ###   ########.fr       */
+/*   Updated: 2024/09/12 10:23:07 by tkartasl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,22 @@ class Request
 	public:
 		Request(std::string request);
 		~Request(void);
-		void	parseMethod(void);
-		void	parseUrl(void);
-		void parseHeaders(void);
+	
+		void parse(void);
+		std::string  getMethod(void) const;
+		std::string  getUrl(void) const;
+		std::string  getBody(void) const;
+		std::string  getHttpVersion(void) const;
+		std::map<std::string, std::string> getHeaders(void) const;
 	private:
 		std::string _request;
 		std::string _method;
 		std::string _url;
 		std::string _body;
+		std::string _httpVersion;
 		std::map<std::string, std::string> _headers;
+		void	_parseRequestLine(void);
+		void 	_parseHeaders(void);
 };
 
 #endif
--- srcs/Request.cpp
+++ srcs/Request.cpp
@@ -6,11 +6,12 @@
 /*   By: tkartasl <tkartasl@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 14:49:12 by tkartasl          #+#    #+#             */
-/*   Updated: 2024/09/10 10:27:25 by tkartasl         ###   ########.fr       */
+/*   Updated: 2024/09/12 10:24:50 by tkartasl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Request.hpp"
+#include "../includes/Request.hpp"
+#include <iostream>
 
 Request::Request(std::string request) : _request(request)
 {
@@ -22,15 +23,16 @@ Request::~Request(void)
 	return;
 }
 
-//Parses method and put's it to string attribute _method, then erases it from the request
-void	Request::parseMethod(void)
+void	Request::_parseRequestLine(void)
 {
+	//Parses method and put's it to string attribute _method, then erases it from the request
+	
 	const char* methods[3] = {"GET", "POST", "DELETE"};
 	int i = this->_request.find_first_of(" ");
 	this->_method = this->_request.substr(0, i);
-	int index = 0;
+	int index;
 	
-	for (index; index < 3; index++)
+	for (index = 0; index < 3; index++)
 	{
 		if (this->_method == methods[i])
 			break;
@@ -38,19 +40,23 @@ void	Request::parseMethod(void)
 	if (index == 3)
 		/*ERROR*/;
 	this->_request.erase(0, this->_method.length() + 1);
-}
 
-//Parses URI and put's it to string attribute _url, then erases it from the request
-void	Request::parseUrl(void)
-{
-	int i = this->_request.find_first_of(" ");
+	//Parses URI and put's it to string attribute _url, then erases it from the request
+	
+	i = this->_request.find_first_of(" ");
 
 	this->_url = this->_request.substr(0, i);
 	this->_request.erase(0, this->_url.length() + 1);
+	
+	//Parses HTTP Veresion nd put's it to string attribute _httpVersion, then erases it from the request
+
+	i = this->_request.find_first_of("\n");
+	this->_httpVersion = this->_request.substr(0, i);
+	this->_request.erase(0, this->_httpVersion.length() + 1);
 }
 
 //Parses headers line by line and adds the header(before :) and value (after :) to map container attribute _headers
-void Request::parseHeaders(void)
+void	Request::_parseHeaders(void)
 {
 	std::string line;
 
@@ -72,4 +78,35 @@ void Request::parseHeaders(void)
 		this->_request.erase(0, lineEnd + 1);
 	}
 	this->_body = this->_request;
+}
+
+void	Request::parse(void)
+{
+	this->_parseRequestLine();
+	this->_parseHeaders();
+}
+
+std::string  Request::getMethod(void) const
+{
+	return this->_method;
+}
+
+std::string  Request::getUrl(void) const
+{
+	return this->_url;
+}
+
+std::string  Request::getBody(void) const
+{
+	return this->_body;
+}
+
+std::string  Request::getHttpVersion(void) const
+{
+	return this->_httpVersion;
+}
+
+std::map<std::string, std::string> Request::getHeaders(void) const
+{
+	return this->_headers;
 }
