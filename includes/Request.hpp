61bf7e4d839267f71b5a11daf353dccc170097fc
Aaro Hamalainen <aaro.h4malainen@gmail.com>
2024-09-12 13:34:26 +0300

    Response class added

--- includes/Request.hpp
+++ includes/Request.hpp
@@ -6,7 +6,7 @@
 /*   By: ahamalai <ahamalai@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 14:30:52 by tkartasl          #+#    #+#             */
-/*   Updated: 2024/09/12 13:29:40 by ahamalai         ###   ########.fr       */
+/*   Updated: 2024/09/12 13:33:56 by ahamalai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ class Request
 		virtual std::string  getBody(void) const;
 		virtual std::string  getHttpVersion(void) const;
 		virtual std::map<std::string, std::string> getHeaders(void) const;
-	private:
+	protected:
 		std::string	_type;
 		std::string _request;
 		std::string _method;
---
+++ includes/Response.hpp
@@ -0,0 +1,16 @@
+#ifndef RESPONSE_HPP
+#define RESPONSE_HPP
+
+#include "Request.hpp"
+
+class Response: public Request
+{
+	public:
+			Response();
+			~Response();
+	private:
+
+};
+
+
+#endif
