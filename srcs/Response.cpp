07ac1de65d4cf38b622004f7850b09ccc18bd774
aaro <aaro.h4malainen@gmail.com>
2024-09-13 14:17:20 +0300

    Made the response code way cleaner

--- srcs/Response.cpp
+++ srcs/Response.cpp
@@ -30,43 +30,33 @@ Response Response::operator=(const Response &input)
 void Response::respond(int clientfd)
 {
 	std::ifstream index;
-	std::string response = "HTTP/1.1 200 OK\r\n";
 
-	if (this->_type == "video/mp4" || this->_type == "image/png")
+
+	std::string response = "HTTP/1.1 200 OK\n";
+	if (this->_url == "/")
 	{
-		std::ifstream index("./www" + this->_url);
-		response += "Content-Type: " + this->_type + "\r\n";
-		response += "Content-Disposition: attachment; filename=\"" + this->_url + "\"\r\n";
-		response += "Content-Length: " + std::to_string(std::filesystem::file_size("./www" + this->_url)) + "\r\n";
-		response += "Keep-Alive: timeout=5, max=100\r\n\r\n";
-
-		send(clientfd, response.c_str(), response.length(), 0);
-		
-		const std::size_t chunkSize = 8192;
-		char buffer[chunkSize];
-		while (index.read(buffer, chunkSize) || index.gcount() > 0) 
-			send(clientfd, buffer, index.gcount(), 0);
-
-		std::cout << "DONE " << std::filesystem::file_size("./www" + this->_url) << " " << "./www" + this->_url << std::endl;
+		this->_url = "/index.html";
+		index.open("./www/index.html");
 	}
 	else
+		index.open("./www" + this->_url);
+	if (index.is_open() == false)
 	{
-		if (this->_url == "/")
-			index.open("./www/index.html");
-		else
-			index.open("./www" + this->_url);
-		if (index.is_open() == false)
-		{
-			index.open("./www/404.html");
-			response = "HTTP/1.1 404 Not Found\r\n";
-		}
-		std::string file;
-		for (std::string line; std::getline(index, line);)
-			file += line;
-		response += "Content-Type: " + this->_type + "\r\n";
-		response += "Content-Length: " + std::to_string(file.length()) + "\r\n";
-		response += "Keep-Alive: timeout=5, max=100\r\n\r\n"; 
-		response += file;
-		send (clientfd, response.c_str(), response.length(), 0);
+		index.open("./www/404.html");
+		response = "HTTP/1.1 404 Not Found\n";
 	}
+	response += "Content-Type: " + this->_type + "\r\n";
+	if (this->_type == "video/mp4" || this->_type == "image/png")
+		response += "Content-Disposition: attachment; filename=\"" + this->_url + "\"\r\n";
+	response += "Content-Length: " + std::to_string(std::filesystem::file_size("./www" + this->_url)) + "\r\n";
+	response += "Keep-Alive: timeout=5, max=100\r\n\r\n";
+
+	send(clientfd, response.c_str(), response.length(), 0);
+	
+	const std::size_t chunkSize = 8192;
+	char buffer[chunkSize];
+	while (index.read(buffer, chunkSize) || index.gcount() > 0) 
+		send(clientfd, buffer, index.gcount(), 0);
+
+	std::cout << "DONE " << std::filesystem::file_size("./www" + this->_url) << " " << "./www" + this->_url << std::endl;
 }
