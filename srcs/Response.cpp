a7f738b06e920cc90c8bd303596f13665efbc4a3
Tomi Kartaslammi <tomi.kartaslammi@gmail.com>
2024-09-13 14:39:39 +0300

    Added type and url incase of 404 error

--- srcs/Response.cpp
+++ srcs/Response.cpp
@@ -44,10 +44,13 @@ void Response::respond(int clientfd)
 	{
 		index.open("./www/404.html");
 		response = "HTTP/1.1 404 Not Found\n";
+		this->_url = "/404.html";
 	}
+	if (this->_type.empty())
+		this->_type = "text/html";
 	response += "Content-Type: " + this->_type + "\r\n";
 	if (this->_type == "video/mp4" || this->_type == "image/png")
-		response += "Content-Disposition: attachment; filename=\"" + this->_url + "\"\r\n";
+		response += "Content-Disposition: attachment; filename=\"" + this->_url + "\r\n";
 	response += "Content-Length: " + std::to_string(std::filesystem::file_size("./www" + this->_url)) + "\r\n";
 	response += "Keep-Alive: timeout=5, max=100\r\n\r\n";
 
