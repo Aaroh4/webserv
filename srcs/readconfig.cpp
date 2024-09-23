7f1061eb49d0fd5c39d714ebcd5ca33fa6705106
Aaro Hamalainen <aaro.h4malainen@gmail.com>
2024-09-23 13:28:15 +0300

    Made reading the config file easily modifiable

--- srcs/readconfig.cpp
+++ srcs/readconfig.cpp
@@ -22,6 +22,45 @@ inline std::string cutFromTo(std::string input, int start, std::string last)
 	return (input.substr(0, input.find(last)));
 }
 
+void locations(std::string temp, ServerInfo &server)
+{
+	std::vector<std::string> location_configs = {"root:", "dir-listing:", "allowed-methods:"};
+	std::unordered_set<std::string> all_methods = 
+	{"GET", "POST", "DELETE", "HEAD"};
+
+	location temploc;
+	
+	temploc.name = temp.substr(0, temp.find(" "));
+	for (size_t i = 0; i < location_configs.size(); i++)
+	{
+		size_t pos = temp.find(location_configs.at(i));
+		if (pos != std::string::npos)
+		{
+			std::string value;
+			value = cutFromTo(temp, pos + location_configs.at(i).size(), "\n");
+			value.erase(std::remove(value.begin(), value.end(), ' '), value.end());
+			switch (i)
+			{
+				case 0:
+						temploc.root = value;
+						break;
+				case 1:
+						if (std::filesystem::is_directory(temploc.root + temploc.name) && value.find("true") != std::string::npos)														// THIS IS FOR TESTING REMEMBER TO SWITCH OUT
+							temploc.dirList = true;
+						else
+							temploc.dirList = false; 
+											break;
+				case 2:
+						for (std::string method : all_methods)
+							if (value.find(method) != std::string::npos)
+								temploc.methods.insert(method);
+						break;
+			}
+		}
+	}
+	server.setnewlocation(temploc);
+}
+
 int brackets(std::string configfile, std::string type, ServerInfo &server)
 {
 	std::string 		temp;
@@ -38,27 +77,7 @@ int brackets(std::string configfile, std::string type, ServerInfo &server)
 	}
 	if (type == "location")
 	{
-		std::unordered_set<std::string> all_methods = 
-		{"GET", "POST", "DELETE", "HEAD"};
-
-		location temploc;		
-		temploc.name = temp.substr(0, temp.find(" "));
-		temploc.root = cutFromTo(temp, temp.find("root: ") + 6, "\n");
-		std::string temp1;
-		if (temp.find("allowedmethods: ") != std::string::npos)
-		{
-			temp1 = cutFromTo(temp, temp.find("allowedmethods: ") + 15, "\n");
-			for (std::string method : all_methods)
-				if (temp1.find(method) != std::string::npos)
-					temploc.methods.insert(method);
-		}
-		//std::cout << "root: " << temploc.root << "\n";
-		if (std::filesystem::is_directory(temploc.root + temploc.name) && cutFromTo(temp, temp.find("dir-listing: "), "\n").find("true") != std::string::npos)														// THIS IS FOR TESTING REMEMBER TO SWITCH OUT
-			temploc.dirList = true; 					// THIS IS FOR TESTING REMEMBER TO SWITCH OUT
-		else											// THIS IS FOR TESTING REMEMBER TO SWITCH OUT
-			temploc.dirList = false; 					// THIS IS FOR TESTING REMEMBER TO SWITCH OUT
-		//std:: cout << "name : " << temploc.name << temploc.dirList << std::endl;
-		server.setnewlocation(temploc);
+		locations(temp, server);
 		config_server(temp.substr(temp.find("{"), temp.size()), server);
 	}
 	else if (type == "server")
@@ -68,35 +87,32 @@ int brackets(std::string configfile, std::string type, ServerInfo &server)
 
 ServerInfo	config_server(std::string temp, ServerInfo &server)
 {
-	std::vector<std::string> string_to_case
+	std::vector<std::string> server_configs
 	{
-		{"host: "},
 		{"host:"},
-		{"port: "},
 		{"port:"},
 		{"location "},
 	};
 
 	server.setsocketfd(socket(AF_INET, SOCK_STREAM, 0));
-	for (size_t j = 0; j < string_to_case.size(); j++)
+	for (size_t j = 0; j < server_configs.size(); j++)
 	{
-		size_t pos = toLowerCase(temp).find(string_to_case.at(j));
+		size_t pos = toLowerCase(temp).find(server_configs.at(j));
 		if (pos != std::string::npos)
 		{
 			std::string	value;
-			std::istringstream stream(temp.substr(pos + string_to_case.at(j).size(), std::string::npos));
-			std::getline(stream, value, '\n');
+			value = cutFromTo(temp.substr(pos + server_configs.at(j).size()), 0, "\n");
+			if (j != 2)
+				value.erase(std::remove(value.begin(), value.end(), ' '), value.end());
 			switch (j)
 			{
 				case 0:
-				case 1:
 					server.set_ip(value);
 					break;
-				case 2:
-				case 3:
+				case 1:
 					server.setnew_port(std::stoi(value));
 					break;
-				 case 4:
+				case 2:
 				 	if (value.find("{") != std::string::npos)
 				 		brackets(temp.substr(temp.find(value), std::string::npos), "location", server);
 					else
@@ -128,9 +144,6 @@ int	readconfig(std::string name, ServerManager &manager)
 		ServerInfo server;
 		temp = temp.substr(brackets(temp, "server", server), std::string::npos);
 		manager.setnew_info(server);
-		//std::cout << server.getlocation() << std::endl;
 	}
-	//std::cout << manager.get_info()[0].getlocation() << std::endl;
-	//std::cout << manager.get_info()[0].get_ip() << std::endl;
 	return (0);
 }
