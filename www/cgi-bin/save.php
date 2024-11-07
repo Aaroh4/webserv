e5c397fc114da06926cf58d759dce0ccd1887df2
Lauri Litovuo <llitovuo@student.hive.fi>
2024-11-07 14:34:58 +0200

    makking the script to make file if it does not exist

--- www/cgi-bin/save.php
+++ www/cgi-bin/save.php
@@ -2,9 +2,15 @@
 
 <?php
 
+$fileToCreate = __DIR__ . '/../data/strings.txt';
+if (!$fileToCreate || !file_exists($fileToCreate)) {
+	if (!touch($fileToCreate)) {
+        die("Failed to create the file.");
+    }
+}
 $file_path = realpath(__DIR__ . '/../data/strings.txt');
 
-if ($file = fopen($file_path, "a")) 
+if ($file = fopen($file_path, "a"))
 {
 	$sanitized_query_string = strip_tags($_SERVER['QUERY_STRING']);
 	$sanitized_query_string = htmlspecialchars($sanitized_query_string, ENT_QUOTES, 'UTF-8');
@@ -14,8 +20,8 @@ if ($file = fopen($file_path, "a"))
 	fwrite($file, "\n");
 	fclose($file);
     echo $name . " written to file.";
-} 
-else 
+}
+else
 {
     echo "Failed to open file for writing.";
 }
